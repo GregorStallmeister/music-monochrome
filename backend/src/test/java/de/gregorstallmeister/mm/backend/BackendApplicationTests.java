6f22d0ed8853606f16680124bf73f62152712773
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-08 17:26:25 +0200

    initial commit.

---
+++ backend/src/test/java/de/gregorstallmeister/mm/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package de.gregorstallmeister.mm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // this method is empty, because it's a dummy for eventually later using.
+    }
+
+}
