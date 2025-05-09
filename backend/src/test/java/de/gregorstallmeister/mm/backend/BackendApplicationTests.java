a2a1b06374c18a9b8970e1bec45e87bc1222327c
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-09 11:54:25 +0200

    remove not running (empty) test.

--- backend/src/test/java/de/gregorstallmeister/mm/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/gregorstallmeister/mm/backend/BackendApplicationTests.java
@@ -6,9 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // this method is empty, because it's a dummy for eventually later using.
-    }
-
 }
