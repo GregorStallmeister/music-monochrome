51a0cf2f8c2f1d5e4a37bfdd9f6bc59d91e6878e
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-13 17:23:06 +0200

    design frontend. add some display elements.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {type ChangeEvent, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [token, setToken] = useState<string>()
+
+    function updateToken(event: ChangeEvent<HTMLInputElement>) {
+        setToken(event.target.value)
+    }
+
+    return (
+        <div className="app">
+            <div className="card">
+                music monochrome
+                &nbsp;
+                Ihr Token bitte:&nbsp;<input name="token" placeholder={"Spotify Zugriffs-Token hier einfügen"} size={75}
+                                             value={token}
+                                             onChange={event => updateToken(event)}/>
+                &nbsp;
+                <button type="submit">&nbsp;anmelden&nbsp;</button>
+                <br/>
+                ================
+            </div>
+        </div>
+    )
 }
 
 export default App
