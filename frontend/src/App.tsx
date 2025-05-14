a27c8d462f12eeacce6eb2591c21522e8e20d1a0
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-14 09:50:39 +0200

    refactor entry in frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
 import './App.css'
-import {type ChangeEvent, useState} from "react";
 
 function App() {
 
-    const [token, setToken] = useState<string>()
-
-    function updateToken(event: ChangeEvent<HTMLInputElement>) {
-        setToken(event.target.value)
-    }
 
     return (
         <div className="app">
             <div className="card">
                 music monochrome
                 &nbsp;
-                Ihr Token bitte:&nbsp;<input name="token" placeholder={"Spotify Zugriffs-Token hier einfügen"} size={75}
-                                             value={token}
-                                             onChange={event => updateToken(event)}/>
-                &nbsp;
-                <button type="submit">&nbsp;anmelden&nbsp;</button>
+                Melde Dich mit Deinem Spotify Account an:&nbsp;<button type="submit">&nbsp;anmelden&nbsp;</button>
                 <br/>
                 ================
             </div>
